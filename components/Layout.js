6bda150f60aea0bf5639f9eeaa17025fe974e89b
Dakota <dakotakcoppage@me.com>
2022-05-17 14:40:05 -0500

    adding components folder with header, nav

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Nav } from './Nav';
+import { Header } from './Header';
+import styles from '../styles/Layout.module.css';
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
